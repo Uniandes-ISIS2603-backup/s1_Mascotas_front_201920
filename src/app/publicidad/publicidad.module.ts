b74e6a6173dfbaa4c8ea1a69021ae1add02524c8
german rozo <gd.rozo@uniandes.edu.co>
2019-10-31 08:34:09 -0500

    modificacion publicidad

--- src/app/publicidad/publicidad.module.ts
+++ src/app/publicidad/publicidad.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicidadListComponent } from './publicidad-list/publicidad-list.component';
 import { PublicidadService } from './publicidad.service';
+import { UsuarioDetalleComponent } from './usuario-detalle/usuario-detalle.component';
 
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [PublicidadListComponent],
+  declarations: [PublicidadListComponent, UsuarioDetalleComponent],
   exports : [PublicidadListComponent],
   providers: [PublicidadService]
 })
