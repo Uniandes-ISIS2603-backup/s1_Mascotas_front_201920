b4d64d2b5f171f25cd9b6b256af7971c89b23962
Javos <javelino2311@gmail.com>
2019-11-07 16:32:39 -0500

    Fixed compiling error

--- src/app/mascotaperdida/mascotaperdida.module.ts
+++ src/app/mascotaperdida/mascotaperdida.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MascotaperdidaListComponent } from './mascotaperdida-list/mascotaperdida-list.component';
+import { MascotaPerdidaListComponent } from './mascotaperdida-list/mascotaperdida-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ MascotaperdidaListComponent],
-  exports: [MascotaperdidaListComponent]
+  declarations: [ MascotaPerdidaListComponent],
+  exports: [MascotaPerdidaListComponent]
 })
 export class MascotaperdidaModule { }
