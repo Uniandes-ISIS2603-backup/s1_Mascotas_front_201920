035f6c8d78c83d35358a70eb10dee7715cf78607
lilyaita <la.duque@uniandes.edu.co>
2019-11-10 23:03:58 -0500

    Documentacion

--- src/app/mascotaperdida/mascotaperdida.ts
+++ src/app/mascotaperdida/mascotaperdida.ts
@@ -1,56 +1,42 @@
 import { Usuario } from "../usuario/usuario";
-export class MascotaPerdida 
-{
-   /**
-     * El usuario asociado a la mascota
-     */
-    
-   usuario?: Usuario;
-    
-    /**
-     * Recompensa
-     */
-   /*RecompensaEntity recompensa;*/
-  
-    /**
-     * La fecha en la que se perdió la mascota
-     */
-    
-    fechaPerdida: Date;
-    /**
-     * Fotos que tiene la mascota
-     */
-    /*@PodamExclude
-    @OneToMany(mappedBy = "mascotaPerdida")
-    private List<MultimediaEntity> fotos = new ArrayList<>();*/
-    
-    /**
-     * Videos que tiene la mascota
-     */
-    /*@PodamExclude
-    @OneToMany(mappedBy = "mascotaPerdida2")
-    private List<MultimediaEntity> videos = new ArrayList<>();*/
+import { Recompensa } from "../recompensa/recompensa";
+export class MascotaPerdida {
+  /**
+    * El usuario asociado a la mascota
+    */
+  usuario?: Usuario;
 
-    /**
-     * Raza de la mascota perdida
-     */
-    raza: string;
-    
-    /**
-     * La especie de la mascota perdida
-     */
-    especie: number;
-    
-    /**
-     * La descripción de la mascota perdida
-     */
-    descripcion: string;
-    
-    /**
-     * El lugar donde se perdió la mascota
-     */
-    lugar: string;
-    
-    
+  /**
+   * Recompensa
+   */
+  recompensa?: Recompensa;
+
+  /**
+   * La fecha en la que se perdió la mascota
+   */
+
+  fechaPerdida: Date;
+
+  /**
+   * Raza de la mascota perdida
+   */
+  raza: string;
+
+  /**
+   * La especie de la mascota perdida
+   */
+  especie: number;
+
+  /**
+   * La descripción de la mascota perdida
+   */
+  descripcion: string;
+
+  /**
+   * El lugar donde se perdió la mascota
+   */
+  lugar: string;
+
+
 
 }
